14bd3aa33d283ab68e0017e5927d532b62d4b31e
Saamson <sunkarasamsonsamson@gmail.com>
2026-08-18 20:32:13 +0530

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {  Route, Routes } from 'react-router-dom';
 import About from './Components/About';
 import Signup from './Components/Signup';
 import Home from './Components/Home';
+import Products from './Components/Products';
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/about"  element={<About/>} />
+          <Route path="/products"  element={<Products/>} />
         </Routes>
     </>
   )
